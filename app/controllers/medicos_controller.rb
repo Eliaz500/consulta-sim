9a887f5fd99cb0dcdaa00894f12b75c0d036f359
Eliaz500 <eliazzlima@gmail.com>
2022-11-28 17:22:31 -0300

    Cadastrar e Visualizar Medico

---
+++ app/controllers/medicos_controller.rb
@@ -0,0 +1,27 @@
+class MedicosController < ApplicationController
+  def index
+    @medicos = Medico.all
+  end
+  def show
+    @medico = Medico.find(params[:id])
+  end
+
+  def new
+    @medico = Medico.new
+  end
+
+  def create
+    @medico = Medico.new(medico_params)
+
+    if @medico.save
+      redirect_to @medico
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def medico_params
+      params.require(:medico).permit(:nome_completo, :cpf, :email, :especialidade, :crm)
+    end
+end
