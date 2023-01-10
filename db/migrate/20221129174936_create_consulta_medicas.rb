988b92897601126f7d79f6d6aea15a35f20b40e7
Eliaz500 <eliazzlima@gmail.com>
2023-01-10 17:51:51 -0300

    Editar consulta medica

--- db/migrate/20221129174936_create_consulta_medicas.rb
+++ db/migrate/20221129174936_create_consulta_medicas.rb
@@ -1,8 +1,8 @@
 class CreateConsultaMedicas < ActiveRecord::Migration[7.0]
   def change
     create_table :consulta_medicas do |t|
-      t.string :data
-      t.string :horario
+      t.date :data
+      t.time :horario
       t.references :paciente, foreign_key: true
       t.references :medico, foreign_key: true
 
