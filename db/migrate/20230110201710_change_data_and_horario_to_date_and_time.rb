59a6a4ef557ff94f195ac96e99a790ba3ba60fad
Eliaz500 <eliazzlima@gmail.com>
2023-01-26 23:32:15 -0300

    Teste cucumber feature paciente

--- db/migrate/20230110201710_change_data_and_horario_to_date_and_time.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDataAndHorarioToDateAndTime < ActiveRecord::Migration[7.0]
-  def change
-    change_column :consulta_medicas, :data, :date
-    change_column :consulta_medicas, :horario, :time
-  end
-end
---
+++ features/support/spec_helper.rb
@@ -0,0 +1,4 @@
+# Capybara.register_driver :chrome do |app|
+#   Capybara::Selenium::Driver.new(app, browser: :chrome, driver_path: "C:/Users/Elias/.local/share/gem/ruby/3.1.0/bin/chromedriver.exe")
+# end
+
